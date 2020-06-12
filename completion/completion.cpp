5983ca1ced654d10070331672ae61958995f81c2
JoshuaKeegan3 <37237625+JoshuaKeegan3@users.noreply.github.com>
2020-06-12 16:58:29 +1200

    Completion

---
+++ completion/completion.cpp
@@ -0,0 +1,76 @@
+#include "robot.hpp"
+#include "LinePos.hpp"
+#include "Speeds.hpp"
+#include "Intercection.hpp"
+#define _USE_MATH_DEFINES
+ 
+#include <cmath>
+
+double calculateOffset(double speed){
+	/// Returns a double between -1 and 1
+	/// Representing the distance the centre
+	/// Of the white line is away from the robot
+	/// If the number is positive the line is to the left
+	/// If the number is negitive the line is to the right
+	for(int row=0; row<cameraView.height;row++)
+	{//turn left
+		if(get_pixel(cameraView,row,0,3)>250){
+			std::cout<<"left"<<std::endl;
+			return row!=0?atan(cameraView.width/2.0/row):1;
+		}
+	}
+	for(int col=0; col<cameraView.width;col++){//adjust forward
+		if(get_pixel(cameraView,0,col,3)>250){
+			std::cout<<"top"<<std::endl;
+			return atan((cameraView.width/2.0-col)/cameraView.height);
+		}
+	}
+	for(int row=0; row<cameraView.height;row++){//turn right
+		if(get_pixel(cameraView,row,cameraView.width-1,3)>250){
+			std::cout<<"right"<<std::endl;
+			return row!=0?-atan(cameraView.width/2.0/row):1;
+		}
+	}
+	for(int col=0; col<cameraView.width;col++){//adjust forward
+		if(get_pixel(cameraView,cameraView.height - 1,col,3)>250){
+			std::cout<<"bot"<<std::endl;
+			return cameraView.width/2.0-col>0?1:0;
+		}
+	}
+	
+
+	return speed * (M_PI_2)/4;
+}
+Speeds calculateWheelSpeeds(double speed){
+	Speeds speeds;
+	double angle = calculateOffset(speed);
+	
+	std::cout<<" angle="<<angle<<std::endl;
+	speeds.vLeft=speed-(angle)*675/speed/2/(M_PI_2);
+	speeds.vRight=speed+(angle)*675/speed/2/(M_PI_2);
+	speeds.vRight=speed+(angle)*675/speed/2/(M_PI_2);
+
+
+	return speeds;
+}
+int main(){
+	if (initClientRobot() !=0){
+		std::cout<<" Error initializing robot"<<std::endl;
+	}
+    double vLeft = 5.0;
+    double vRight = 5.0;
+    double speed = 20.0;
+    takePicture();
+    SavePPMFile("i0.ppm",cameraView);
+    while(1){
+	  takePicture();
+	  Speeds speeds = calculateWheelSpeeds(speed);
+	  vLeft = speeds.vLeft;
+	  vRight = speeds.vRight;
+      setMotors(vLeft,vRight);   
+      std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
+       usleep(100000);
+  } //while
+
+} // main
+
